36de631e2b1588da5d307c175fe3e59934b1a884
Victor Ramirez <VRamirez75@gmail.com>
2019-07-18 22:10:00 -0700

    Updated the app.js.

--- app.js
+++ app.js
@@ -17,72 +17,23 @@ app.get("/", function(req, res){
 });
 
 // creating a “route”
-app.get("/mercury", function(req, res){
+app.get("/artificial_intelligence", function(req, res){
 
-    res.render("mercury.html");
-
-});
-
-// creating a “route”
-app.get("/venus", function(req, res){
-
-    res.render("venus.html");
+    res.render("artificial_intelligence.html");
 
 });
 
 // creating a “route”
-app.get("/earth", function(req, res){
+app.get("/machine_learning", function(req, res){
 
-    res.render("earth.html");
-
-});
-
-// creating a “route”
-app.get("/mars", function(req, res){
-
-    res.render("mars.html");
+    res.render("machine_learning.html");
 
 });
 
 // creating a “route”
-app.get("/jupiter", function(req, res){
+app.get("/deep_learning", function(req, res){
 
-    res.render("jupiter.html");
-
-});
-
-// creating a “route”
-app.get("/saturn", function(req, res){
-
-    res.render("saturn.html");
-
-});
-
-// creating a “route”
-app.get("/uranus", function(req, res){
-
-    res.render("uranus.html");
-
-});
-
-// creating a “route”
-app.get("/neptune", function(req, res){
-
-    res.render("neptune.html");
-
-});
-
-// creating a “route”
-app.get("/pluto", function(req, res){
-
-    res.render("pluto.html");
-
-});
-
-// creating a “route”
-app.get("/planet9", function(req, res){
-
-    res.render("planet9.html");
+    res.render("deep_learning.html");
 
 });
 
